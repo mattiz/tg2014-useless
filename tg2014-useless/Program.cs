d695db504fed35cb381f6b4b5c53c6e57f43ba62
Mathias Bjerke <mathias@verida.no>
2014-04-17 15:30:14 +0200

    Initial commit

---
+++ tg2014-useless/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+ 
+namespace MyTrayApp
+{
+    public class SysTrayApp : Form
+    {
+        [STAThread]
+        public static void Main()
+        {
+            Application.Run(new SysTrayApp());
+        }
+ 
+        private NotifyIcon  trayIcon;
+        private ContextMenu trayMenu;
+ 
+        public SysTrayApp()
+        {
+            trayMenu = new ContextMenu();
+            trayMenu.MenuItems.Add("Exit", OnExit);
+
+            trayIcon      = new NotifyIcon();
+            trayIcon.Text = "MyTrayApp";
+            trayIcon.Icon = new Icon(SystemIcons.Application, 40, 40);
+
+            trayIcon.ContextMenu = trayMenu;
+            trayIcon.Visible     = true;
+        }
+ 
+        protected override void OnLoad(EventArgs e)
+        {
+            Visible = false;
+            ShowInTaskbar = false;
+ 
+            base.OnLoad(e);
+        }
+ 
+        private void OnExit(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+ 
+        protected override void Dispose(bool isDisposing)
+        {
+            if (isDisposing)
+            {
+                trayIcon.Dispose();
+            }
+ 
+            base.Dispose(isDisposing);
+        }
+    }
+}
